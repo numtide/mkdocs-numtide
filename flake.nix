9f800b2665fea91c7dfa1f11dc6f296d5a89473e
zimbatm <zimbatm@zimbatm.com>
2023-03-24 11:36:45 +0100

    wrap things in nix
    Since we are not publishing our own template, but wrapping an existing
    one, here is the approach that I'm taking:
    
    Split the config of this repo in two:
    
    * base.yml is what is going to be used by the consumers
    * mkdocs.yml is for the local demo. INHERIT: base.yml
    
    Then wrap mkdocs so that the MKDOCS_NUMTIDE_THEME points to the repo's base.yml
    
    Then on the consumer side we can use `INHERIT: ENV! MKDOCS_NUMTIDE_THEME` to load the base config.

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "mkdocs-numtide";
+
+  outputs = { self, nixpkgs }:
+    let
+      eachSystem = f:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system:
+          f nixpkgs.legacyPackages.${system}
+        );
+    in
+    {
+      packages = eachSystem (pkgs: {
+        default = import ./. { inherit pkgs; };
+
+        docs = self.lib.${pkgs.system}.mkDocs {
+          name = "mkdocs-numtide";
+          src = ./.;
+        };
+      });
+
+      lib = eachSystem (pkgs: {
+        mkDocs =
+          # Function that builds the docs for a repo.
+          #
+          # It assumes that we want to import the src/mkdocs.yml and all of
+          # the src/docs folder.
+          { name, src }:
+          pkgs.stdenv.mkDerivation {
+            inherit name;
+
+            src = builtins.path {
+              name = "src-${name}-docs";
+              path = src + /docs;
+            };
+
+            # Re-create the folder structure since mkdocs insists on having the
+            # mkdocs.yml at the root of the repo.
+            unpackPhase = ''
+              cp -r --no-preserve=mode $src docs
+              cp ${src + /mkdocs.yml} mkdocs.yml
+            '';
+
+            nativeBuildInputs = [
+              self.packages.${pkgs.system}.default
+            ];
+
+            buildPhase = ''
+              mkdocs build
+            '';
+
+            installPhase = ''
+              mv site $out
+            '';
+          };
+      });
+
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShellNoCC {
+          packages = [
+            self.packages.${pkgs.system}.default
+          ];
+        };
+      });
+    };
+}
